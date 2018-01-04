e4f1b3840f442675bdc39b2a9153e74869a56be0
nikhiltri <ntrivedi@artic.edu>
2018-01-04 17:03:15 -0600

    Create LOC ID file to ingest into Blazegraph

--- initialize/extract-ulan-triples.sh
+++ initialize/extract-ulan-triples.sh
@@ -2,6 +2,7 @@
 
 DIR=`pwd`
 
+grep '\(http://www.w3.org/2004/02/skos/core#exactMatch\|http://www.w3.org/2004/02/skos/core#closeMatch\).*loc.gov' ../source-data/ulan/ULANOut_Full.nt > ../source-data/ulan/ULAN_LOCIDs.nt
 grep http://www.w3.org/2004/02/skos/core#prefLabel ../source-data/ulan/ULANOut_Full.nt > ../source-data/ulan/ULAN_PrefLabels.nt
 grep http://www.w3.org/2004/02/skos/core#altLabel ../source-data/ulan/ULANOut_Full.nt > ../source-data/ulan/ULAN_AltLabels.nt
 grep http://www.w3.org/2004/02/skos/core#related ../source-data/ulan/ULANOut_Full.nt > ../source-data/ulan/ULAN_Related.nt
